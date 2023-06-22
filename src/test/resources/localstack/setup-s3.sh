dda6f325c6cb665a2ae2efd78b66add4b95c1c60
tim <wright.tim.j@gmail.com>
2023-06-22 16:12:14 +0100

    working version

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -6,11 +6,10 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
-echo "S3 Configuration started 22"
+echo "S3 Configuration started"
 
 ls
 aws --endpoint-url=http://localhost:4566 s3 mb s3://mojap-adjudications-insights
-#aws --endpoint-url=http://localhost:4566 s3 cp /tmp/test-data s3://mojap-adjudications-insights
-#aws --endpoint-url=http://localhost:4566 s3 cp src/test/resources/test-data/ s3://mojap-adjudications-insights/chart
+aws --endpoint-url=http://localhost:4566 s3 cp /tmp/localstack/test-data/ s3://mojap-adjudications-insights --recursive
 
 echo "S3 Configured"
