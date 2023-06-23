8120b4fe7b62acaa7ad4c03dc85d86fd4a8c1cbc
tim <wright.tim.j@gmail.com>
2023-06-23 16:57:43 +0100

    wip

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -3,6 +3,9 @@
 set -e
 export TERM=ansi
 export AWS_DEFAULT_REGION=eu-west-2
+export AWS_ACCESS_KEY_ID=foobar
+export AWS_SECRET_ACCESS_KEY=foobar
+
 echo "S3 Configuration started"
 
 aws --endpoint-url=http://localhost:4566 s3 mb s3://mojap-adjudications-insights
