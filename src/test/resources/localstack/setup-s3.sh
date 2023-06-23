0cd33788554b619b86c569e96f87e1fa43dd4b47
Dmitri Algazin <135022118+algazin-moj@users.noreply.github.com>
2023-06-23 17:20:20 +0100

    NN-5219 - Back End - Create first chart API; - LOCALSTACK_TMP_FOLDER;

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -6,8 +6,12 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 
+export LOCALSTACK_TMP_FOLDER="${LOCALSTACK_TMP_FOLDER:=/tmp/localstack/test-data/}"
+
 echo "S3 Configuration started"
 
+echo "LOCALSTACK_TMP_FOLDER=${LOCALSTACK_TMP_FOLDER}"
+
 aws --endpoint-url=http://localhost:4566 s3 mb s3://mojap-adjudications-insights
-aws --endpoint-url=http://localhost:4566 s3 cp ./src/test/resources/test-data/ s3://mojap-adjudications-insights --recursive
+aws --endpoint-url=http://localhost:4566 s3 cp ${LOCALSTACK_TMP_FOLDER} s3://mojap-adjudications-insights --recursive
 echo "S3 Configured"
