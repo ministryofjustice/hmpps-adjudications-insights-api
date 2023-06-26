c62fd31843e87249d9c57c7f35325c1d1839a803
Tim Wright <wright.tim.j@gmail.com>
2023-06-26 09:14:02 +0100

    int test setup (#11)
    * int test setup
    
    * reformat
    
    * inc timeout
    
    * testing
    
    * NN-5219 - Back End - Create first chart API;
    - update test chart data for all available charts;
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wp
    
    * wip
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * wip
    
    * test
    
    * NN-5219 - Back End - Create first chart API;
    - LOCALSTACK_TMP_FOLDER;
    
    * Updated config.yml
    
    * Updated config.yml
    
    ---------
    
    Co-authored-by: Dmitri Algazin <135022118+algazin-moj@users.noreply.github.com>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/integration/IntegrationTestBase.kt
@@ -1,12 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.integration
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "36000")
 @ActiveProfiles("test")
 abstract class IntegrationTestBase {
 
