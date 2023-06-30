da10aae2df4548d56a3bf68aba93871a1e764623
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-06-30 14:48:17 +0100

    NN-5269: S3 bucket in dev (#14)
    * NN-5269: configure serviceAccountName in dev
    
    * remove credentials
    
    * config
    
    * config
    
    * config
    
    ---------
    
    Co-authored-by: tim <wright.tim.j@gmail.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
 
-import com.amazonaws.auth.InstanceProfileCredentialsProvider
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.context.annotation.Bean
@@ -12,13 +12,5 @@ import org.springframework.context.annotation.Profile
 class AwsConfig {
 
   @Bean
-  fun amazonS3(): AmazonS3? {
-    return AmazonS3ClientBuilder.standard()
-      .withCredentials(InstanceProfileCredentialsProvider(false))
-      // .withRegion(Regions.EU_WEST_2)
-      //    .withEndpointConfiguration(
-      //      AwsClientBuilder.EndpointConfiguration("http://s3.localhost.localstack.cloud:4566", "eu-west-2")
-      //    )
-      .build()
-  }
+  fun amazonS3(): AmazonS3? = AmazonS3ClientBuilder.standard().withRegion(Regions.EU_WEST_2).build()
 }
