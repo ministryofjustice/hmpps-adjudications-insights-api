56db3b2769b7b811b40a1cad7b2edb61e4c6cefc
tim <wright.tim.j@gmail.com>
2023-06-30 13:15:41 +0100

    remove credentials

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
 
-import com.amazonaws.auth.InstanceProfileCredentialsProvider
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.context.annotation.Bean
@@ -14,11 +13,6 @@ class AwsConfig {
   @Bean
   fun amazonS3(): AmazonS3? {
     return AmazonS3ClientBuilder.standard()
-      .withCredentials(InstanceProfileCredentialsProvider(false))
-      // .withRegion(Regions.EU_WEST_2)
-      //    .withEndpointConfiguration(
-      //      AwsClientBuilder.EndpointConfiguration("http://s3.localhost.localstack.cloud:4566", "eu-west-2")
-      //    )
       .build()
   }
 }
