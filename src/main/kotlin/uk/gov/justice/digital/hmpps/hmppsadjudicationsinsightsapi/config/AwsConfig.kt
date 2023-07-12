02f77d5c3b491540ae87c6aeae064fa2bfc5e352
tim <wright.tim.j@gmail.com>
2023-07-12 12:11:52 +0100

    ktlint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfig.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
 
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import org.springframework.beans.factory.annotation.Value
