bc7f532be92557913d5d314776f2264d05e38f5e
Tim Wright <wright.tim.j@gmail.com>
2023-07-12 17:51:51 +0100

    add sts back (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
   implementation("com.google.code.gson:gson:2.10.1")
   implementation("com.amazonaws:aws-java-sdk-s3:1.12.503")
+  implementation("com.amazonaws:aws-java-sdk-sts:1.12.503")
 
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
