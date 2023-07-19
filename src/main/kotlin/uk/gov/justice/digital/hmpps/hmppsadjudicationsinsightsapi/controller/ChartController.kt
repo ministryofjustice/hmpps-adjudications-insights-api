39b740bddd610137b0ef6a229132fecd3b8321c2
Dmitri Algazin <135022118+algazin-moj@users.noreply.github.com>
2023-07-19 12:23:23 +0100

    NN-5311: 5c - Adjudications resolved with more than one hearing - current month and previous 12 months; - 5c chart added; - 'characteristic' query param removed from API as not been used;

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/controller/ChartController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/controller/ChartController.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.dtos.Chart
 import uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.dtos.ChartDataResponseDto
@@ -17,19 +16,15 @@ class ChartController(private val chartService: ChartService) {
   fun getChart(
     @PathVariable(name = "agencyId") agencyId: String,
     @PathVariable(name = "chartName") chartName: String,
-    @RequestParam
-    (name = "characteristic", required = false) characteristic: String?,
   ): ChartDataResponseDto {
     val chart = chartService.getChart(
       agencyId = agencyId,
       chart = Chart.getChart(chartName),
-      characteristic = characteristic,
     )
     return ChartDataResponseDto(
       agencyId = agencyId,
       chartName = chartName,
       chartEntries = chart,
-      characteristic = characteristic,
     )
   }
 }
