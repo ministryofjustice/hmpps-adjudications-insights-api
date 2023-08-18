08ed158cbf559c6ea94ab993eb118e900621f44a
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2023-08-18 13:46:31 +0100

    NN-5419: add roles to endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/controller/ChartController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/controller/ChartController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.controller
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +11,7 @@ import uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.dtos.ChartMeta
 import uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.service.ChartService
 
 @RestController
+@PreAuthorize("hasRole('VIEW_ADJUDICATIONS')")
 @RequestMapping("/api/data-insights/chart")
 class ChartController(private val chartService: ChartService) {
 
