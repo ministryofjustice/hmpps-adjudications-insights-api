c0464f647d263f05c1bbf848826cb0e7b6954215
Tim Wright <wright.tim.j@gmail.com>
2024-02-06 09:20:29 +0000

    Revert "NN-5849 cache experiment (#70)"
    This reverts commit 945f35abb6a8e2bd374190ba059872576a69d2a3.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
@@ -2,9 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
-@EnableCaching
 @SpringBootApplication()
 class HmppsAdjudicationsInsightsApi
 
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/service/S3Facade.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/service/S3Facade.kt
@@ -5,9 +5,6 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cache.annotation.CacheEvict
-import org.springframework.cache.annotation.Cacheable
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,27 +13,15 @@ class S3Facade(
   @Value("\${data-insights.bucket.name}") private val bucketName: String,
 ) {
 
-  @Cacheable("insights")
   fun getFile(fileName: String): String {
     log.info("getting $bucketName $fileName")
+
     return amazonS3.getObjectAsString(bucketName, fileName)
   }
 
-  @Cacheable("insights-meta")
   fun getS3ObjectMetadata(fileName: String): ObjectMetadata {
     return amazonS3.getObjectMetadata(bucketName, fileName)
   }
-
-  @Scheduled(cron = "@hourly")
-  private fun evictCaches() {
-    evict()
-  }
-
-  @CacheEvict(value = ["insights", "insights-meta"], allEntries = true)
-  private fun evict() {
-    log.info("caches evicted")
-  }
-
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
