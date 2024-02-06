945f35abb6a8e2bd374190ba059872576a69d2a3
Tim Wright <wright.tim.j@gmail.com>
2024-02-06 09:19:10 +0000

    NN-5849 cache experiment (#70)
    * NN-5849 test for memory issues
    
    * NN-5849 caching test
    
    * format
    
    * use localstack 3
    
    * confirming cache is causing test issue
    
    * disable cache for testing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
@@ -2,7 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+@EnableCaching
 @SpringBootApplication()
 class HmppsAdjudicationsInsightsApi
 
