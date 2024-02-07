66a549e26ccbeaf1a4152eb16213e272f458fb5d
Tim Wright <wright.tim.j@gmail.com>
2024-02-07 09:57:56 +0000

    NN-5855 use cache names (#76)
    * NN-5855 use cache names
    
    * move logger

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/HmppsAdjudicationsInsightsApi.kt
@@ -1,15 +1,29 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @EnableScheduling
 @EnableCaching
-@SpringBootApplication()
-class HmppsAdjudicationsInsightsApi
+@SpringBootApplication
+class HmppsAdjudicationsInsightsApi {
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
 
-fun main(args: Array<String>) {
-  runApplication<HmppsAdjudicationsInsightsApi>(*args)
+  fun main(args: Array<String>) {
+    runApplication<HmppsAdjudicationsInsightsApi>(*args)
+  }
+
+  @Scheduled(cron = "@hourly")
+  @CacheEvict(cacheNames = ["chart-meta", "charts"], allEntries = true)
+  fun evictCaches() {
+    log.info("evicting all caches")
+  }
 }
