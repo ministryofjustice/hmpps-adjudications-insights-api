4fffa73089043b3c82496932988854822b56dd19
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-03 12:34:36 +0000

    updated springboot and swagger versions to solve vulnerability

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfigLocalstack.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AwsConfigLocalstack.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
 
-import com.amazonaws.ClientConfiguration
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
@@ -14,16 +13,10 @@ class AwsConfigLocalstack {
 
   @Bean
   fun amazonS3(): AmazonS3? {
-    // Disable chunked encoding to avoid sending x-amz-trailer header
-    val clientConfiguration = ClientConfiguration()
-
     return AmazonS3ClientBuilder.standard()
       .withEndpointConfiguration(
         AwsClientBuilder.EndpointConfiguration("http://s3.localhost.localstack.cloud:4566", "eu-west-1"),
       )
-      .withClientConfiguration(clientConfiguration)
-      .withPathStyleAccessEnabled(true) // recommended for LocalStack
-      .disableChunkedEncoding() // disable chunked encoding
       .build()
   }
 }
