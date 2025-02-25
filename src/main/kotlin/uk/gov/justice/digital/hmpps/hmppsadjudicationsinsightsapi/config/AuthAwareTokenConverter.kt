fbddefdb3ae400e301d718743d876d960287d386
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-25 17:42:24 +0000

    updating springboot version to fix vulnerability

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AuthAwareTokenConverter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsadjudicationsinsightsapi/config/AuthAwareTokenConverter.kt
@@ -1,57 +1,3 @@
-//package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
-//
-//import org.springframework.core.convert.converter.Converter
-//import org.springframework.security.authentication.AbstractAuthenticationToken
-//import org.springframework.security.core.GrantedAuthority
-//import org.springframework.security.core.authority.SimpleGrantedAuthority
-//import org.springframework.security.oauth2.jwt.Jwt
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-//import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
-//
-//class AuthAwareTokenConverter : Converter<Jwt, AbstractAuthenticationToken> {
-//  private val jwtGrantedAuthoritiesConverter: Converter<Jwt, Collection<GrantedAuthority>> =
-//    JwtGrantedAuthoritiesConverter()
-//
-//  override fun convert(jwt: Jwt): AbstractAuthenticationToken {
-//    val claims = jwt.claims
-//    val userName = findUserName(claims)
-//    val clientId = findClientId(claims)
-//    val authorities = extractAuthorities(jwt)
-//    return AuthAwareAuthenticationToken(jwt, userName, clientId, authorities)
-//  }
-//
-//  private fun findUserName(claims: Map<String, Any?>): String? {
-//    return if (claims.containsKey("user_name")) {
-//      claims["user_name"] as String
-//    } else {
-//      null
-//    }
-//  }
-//
-//  private fun findClientId(claims: Map<String, Any?>) =
-//    claims["client_id"] as String
-//
-//  private fun extractAuthorities(jwt: Jwt): Collection<GrantedAuthority> {
-//    val authorities = mutableListOf<GrantedAuthority>().apply { addAll(jwtGrantedAuthoritiesConverter.convert(jwt)!!) }
-//    if (jwt.claims.containsKey("authorities")) {
-//      @Suppress("UNCHECKED_CAST")
-//      val claimAuthorities = (jwt.claims["authorities"] as Collection<String>).toList()
-//      authorities.addAll(claimAuthorities.map(::SimpleGrantedAuthority))
-//    }
-//    return authorities.toSet()
-//  }
-//}
-//
-//class AuthAwareAuthenticationToken(
-//  jwt: Jwt,
-//  val userName: String?,
-//  val clientId: String,
-//  authorities: Collection<GrantedAuthority>,
-//) : JwtAuthenticationToken(jwt, authorities) {
-//  override fun getPrincipal(): Any {
-//    return userName ?: clientId
-//  }
-//}
 package uk.gov.justice.digital.hmpps.hmppsadjudicationsinsightsapi.config
 
 import org.springframework.core.convert.converter.Converter
@@ -74,20 +20,26 @@ class AuthAwareTokenConverter : Converter<Jwt, AbstractAuthenticationToken> {
     return AuthAwareAuthenticationToken(jwt, userName, clientId, authorities)
   }
 
-  private fun findUserName(claims: Map<String, Any?>): String? =
-    if (claims.containsKey("user_name")) claims["user_name"] as String else null
-
-  private fun findClientId(claims: Map<String, Any?>) = claims["client_id"] as String
-
-  private fun extractAuthorities(jwt: Jwt): Collection<GrantedAuthority> =
-    run {
-      val baseAuthorities = jwtGrantedAuthoritiesConverter.convert(jwt) ?: emptyList()
-      val extra = if (jwt.claims.containsKey("authorities")) {
-        @Suppress("UNCHECKED_CAST")
-        (jwt.claims["authorities"] as Collection<String>).map(::SimpleGrantedAuthority)
-      } else emptyList()
-      (baseAuthorities + extra).toSet()
+  private fun findUserName(claims: Map<String, Any?>): String? {
+    return if (claims.containsKey("user_name")) {
+      claims["user_name"] as String
+    } else {
+      null
     }
+  }
+
+  private fun findClientId(claims: Map<String, Any?>) =
+    claims["client_id"] as String
+
+  private fun extractAuthorities(jwt: Jwt): Collection<GrantedAuthority> {
+    val authorities = mutableListOf<GrantedAuthority>().apply { addAll(jwtGrantedAuthoritiesConverter.convert(jwt)!!) }
+    if (jwt.claims.containsKey("authorities")) {
+      @Suppress("UNCHECKED_CAST")
+      val claimAuthorities = (jwt.claims["authorities"] as Collection<String>).toList()
+      authorities.addAll(claimAuthorities.map(::SimpleGrantedAuthority))
+    }
+    return authorities.toSet()
+  }
 }
 
 class AuthAwareAuthenticationToken(
@@ -96,5 +48,7 @@ class AuthAwareAuthenticationToken(
   val clientId: String,
   authorities: Collection<GrantedAuthority>,
 ) : JwtAuthenticationToken(jwt, authorities) {
-  override fun getPrincipal(): Any = userName ?: clientId
+  override fun getPrincipal(): Any {
+    return userName ?: clientId
+  }
 }
