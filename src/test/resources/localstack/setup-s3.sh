af510fd7603d271cd1479e462459f1e1ef581d16
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-25 14:45:48 +0000

    updating springboot version to fix vulnerability

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -8,6 +8,7 @@ export AWS_SECRET_ACCESS_KEY=foobar
 
 #export PROVIDER_OVERRIDE_S3=v3
 #export AWS_S3_DISABLE_CHUNKED_ENCODING=true
+export S3_SKIP_CHECKSUM_VALIDATION=true
 
 export LOCALSTACK_TMP_FOLDER="${LOCALSTACK_TMP_FOLDER:=/tmp/localstack/test-data/}"
 
