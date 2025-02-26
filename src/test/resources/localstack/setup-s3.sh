1d619d78ce7c06378347b4217922c9cf0f36aecd
Chux <chukwudi.ugorji@digital.justice.gov.uk>
2025-02-26 11:46:40 +0000

    updating springboot version to fix vulnerability

--- src/test/resources/localstack/setup-s3.sh
+++ src/test/resources/localstack/setup-s3.sh
@@ -7,7 +7,6 @@ export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 
 export PROVIDER_OVERRIDE_S3=stream
-#export AWS_S3_DISABLE_CHUNKED_ENCODING=true
 export S3_SKIP_CHECKSUM_VALIDATION=true
 
 export LOCALSTACK_TMP_FOLDER="${LOCALSTACK_TMP_FOLDER:=/tmp/localstack/test-data/}"
@@ -21,16 +20,11 @@ aws configure set default.s3.signature_version s3v4
 
 aws --endpoint-url=http://localhost:4566 s3 mb s3://mojap-adjudications-insights
 
-#aws --endpoint-url=http://localhost:4566 s3api put-object \
-#    --bucket mojap-adjudications-insights \
-#    --key reserve/4b.json \
-#    --body "${LOCALSTACK_TMP_FOLDER}/chart/4b.json"
+aws --endpoint-url=http://localhost:4566 s3api put-object \
+    --bucket mojap-adjudications-insights \
+    --key chart/4b.json \
+    --body "${LOCALSTACK_TMP_FOLDER}/chart/4b.json"
 
-#aws --debug --endpoint-url=http://localhost:4566 s3 cp ${LOCALSTACK_TMP_FOLDER} s3://mojap-adjudications-insights --recursive
-aws --endpoint-url=http://localhost:4566 s3 cp ${LOCALSTACK_TMP_FOLDER} s3://mojap-adjudications-insights --recursive
-
-
-#echo "Verification:"
-#aws --endpoint-url=http://localhost:4566 s3 ls s3://mojap-adjudications-insights/ --recursive
+aws --endpoint-url=http://localhost:4566 s3 cp ${LOCALSTACK_TMP_FOLDER} s3://mojap-adjudications-insights --recursive --exclude "chart/4b.json"
 
 echo "S3 Configured"
