4e1172a56c161e32724358015b1df0b76a9e3648
cugorji-justice <chukwudi.ugorji@digital.justice.gov.uk>
2026-07-07 09:18:28 +0100

    IR-1742: Resolve Snyk vulnerabilities (5 critical, 24 high fixable) (#161)
    * Bumped uk.gov.justice.hmpps.gradle-spring-boot to 10.5.4 and removed Removed the stale jackson-core pin
    
    * Added constraints for spring-retry:2.0.13 (CVE-2026-41710) and opentelemetry-api:1.62.0 (CVE-2026-45292)
    
    * Bumped hmpps-kotlin-spring-boot-starter, hmpps-sqs-spring-boot-starter, hmpps-kotlin-spring-boot-starter-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.4"
   kotlin("plugin.spring") version "2.3.21"
 }
 
@@ -9,9 +9,9 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
   all {
     resolutionStrategy.eachDependency {
-      if (requested.group == "com.fasterxml.jackson.core" && requested.name == "jackson-core") {
-        useVersion("2.21.3")
-        because("Fix GHSA-72hv-8253-57qq: jackson-core async parser DoS")
+      if (requested.group == "io.opentelemetry" && requested.name == "opentelemetry-api") {
+        useVersion("1.62.0")
+        because("CVE-2026-45292: resource allocation without limits, fixed in 1.62.0")
       }
     }
   }
@@ -23,9 +23,18 @@ repositories {
 }
 
 dependencies {
+  constraints {
+    implementation("org.springframework.retry:spring-retry:2.0.13") {
+      because("CVE-2026-41710: resource allocation without limits, fixed in 2.0.13")
+    }
+    implementation("io.opentelemetry:opentelemetry-api:1.62.0") {
+      because("CVE-2026-45292: resource allocation without limits, fixed in 1.62.0")
+    }
+  }
+
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.4.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.5.0")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
@@ -38,9 +47,9 @@ dependencies {
   implementation("com.amazonaws:aws-java-sdk-sts:1.12.797")
 
   // AWS
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.3.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.0")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.4.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.5.0")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
